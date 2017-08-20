0d0d542de0cb75b4e9ae364fa576af92e69a7ac9
Moshe Maizels <moshemaizels@gmail.com>
2017-08-20 23:59:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cart = [];
 
+
 function getCart() {
  return cart;
 }
@@ -11,11 +12,36 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 100)
+ var newItem = {[item]: price}
+ cart.push(newItem)
+ console.log(`${item} has been added to your cart.`)
+ return cart
+
 }
 
 function viewCart() {
   // write your code here
-}
+  var cartView = "In your cart,"
+  if (cart.length == 0) {
+    console.log("Your shopping cart is empty.")
+
+  } if (cart.length == 1) {
+    var obj = cart[0]
+    var objKey = Object.keys(cart[0])
+    console.log(cartView + ` you have ${objKey} at $${obj[objKey]}.`)
+
+  } else {
+    for (var i = 0; i < cart.length; i++) {
+      if (cart.length + 1 === cart.length) {
+        var obj = cart[i]
+        var objKey = Object.keys(cart[i])
+        cartView = cartView + `and ${objKey} at $${obj[objKey].}`
+        console.log(cartView)
+      }
+    }
+  }
+ }
 
 function total() {
   // write your code here
