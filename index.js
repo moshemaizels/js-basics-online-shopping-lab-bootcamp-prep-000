6aae6a118e3ad0300792ff3411661db23ba69f23
Moshe Maizels <moshemaizels@gmail.com>
2017-08-21 01:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function removeFromCart(item) {
        if (obj.hasOwnProperty(item) === true) {
          cart.splice(i, 1)
          return cart
-         
+
        }
 
     }
@@ -84,6 +84,6 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber === undefined) {
-    
+    console.log("Sorry we don't have a credit card on file for you")
   }
 }
